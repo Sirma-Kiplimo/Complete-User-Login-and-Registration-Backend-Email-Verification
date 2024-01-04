8f558929e6b1d11edce9cfa2b7ee5ba05467be87
Sirma-cloud <kevinkiplimo99@gmail.com>
2024-01-04 13:44:41 +0300

    Bootstrapped project from spring initializr

---
+++ src/main/java/com/example/Userloginregistration/EmailVerification/UserLoginRegistrationEmailVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Userloginregistration.EmailVerification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginRegistrationEmailVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginRegistrationEmailVerificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Userloginregistration/EmailVerification/UserLoginRegistrationEmailVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Userloginregistration.EmailVerification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginRegistrationEmailVerificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
